6f50fbdf36012f15a97afc5bfc13365f68e6e27a
ccshuang <tcc.soar@gmail.com>
2017-03-02 17:33:23 +0800

    增加hello.sh的运行日志

--- t/hello.sh
+++ t/hello.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cat /etc/passwd |awk -F':' '{print "Hello,"$1",Your UID is "$3}END{print "Users:"NR}'
+cat /etc/passwd |awk -F':' '{print "Hello,"$1",Your UID is "$3}END{print "Users:"NR}' >> hello.log
 
